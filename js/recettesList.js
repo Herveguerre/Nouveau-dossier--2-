ef3f6743bed527567f51d970d5de63d61ca8852a
herve guerre <herveguerre@hotmail.fr>
2023-06-13 11:57:09 +0200

    maj

---
+++ js/recettesList.js
@@ -0,0 +1,38 @@
+
+export const recettesList = [
+	{
+		imgSrc: "../img/recettes_salades/salade_melon_avocat/assiette.jpg",
+		duration: "10.00",
+		recetteTitle : "Salade melon avocat",
+		descriptionRecette : "Salade d'été gourmande ",
+		etape1: "_ 1 avocats<br>_\\1/2<br>melon<br>_\\250g<br>de<br>tomates cerises<br>_ 20 billes de mozzarellas<br>_ 1 concombre<br>_ 4 oeufs dur",
+
+		img2Src:"../img/recettes_salades/salade_melon_avocat/presentation.png",
+		etape2:" Dans la chair du melon et à l'aide d'une cuillère parisienne, prélevez des petites boules. Vous pouvez également couper le melon en dés. Déposez ces morceaux dans une passoire en plastique en attendant de préparer le reste des légumes.",
+		etape3:"Coupez l'avocat en deux, ôtez le noyau et la peu et détaillez en lanières. Arrosez de jus de citron pour éviter qu'il ne noircisse.",
+		img3Src:"../img/recettes_salades/salade_melon_avocat/final.jpg",
+		etape4:"Coupez les tomates cerises en 2 ou laissez-les tel quel si elles sont trop petites.Dans une assiette, déposez au centre les boules de melon tout autour les morceaux d'avocat et terminez par les tomates.",
+		img4Src:"../img/recettes_salades/salade_melon_avocat/assiette.jpg",
+		etape5:"il n'y plus qu'à deguster ...",
+	},
+	{
+		imgSrc: "../img/recettes_salades/salade_melon_avocat/assiette.jpg",
+		duration: "10.00",
+		recetteTitle : "Salade melon avocat",
+		descriptionRecette : "Salade d'été gourmande ",
+		etape1: "_ 1 avocats<br>_\\1/2<br>melon<br>_\\250g<br>de<br>tomates cerises<br>_ 20 billes de mozzarellas<br>_ 1 concombre<br>_ 4 oeufs dur",
+
+		img2Src:"../img/recettes_salades/salade_melon_avocat/presentation.png",
+		etape2:" Dans la chair du melon et à l'aide d'une cuillère parisienne, prélevez des petites boules. Vous pouvez également couper le melon en dés. Déposez ces morceaux dans une passoire en plastique en attendant de préparer le reste des légumes.",
+		etape3:"Coupez l'avocat en deux, ôtez le noyau et la peu et détaillez en lanières. Arrosez de jus de citron pour éviter qu'il ne noircisse.",
+		img3Src:"../img/recettes_salades/salade_melon_avocat/final.jpg",
+		etape4:"Coupez les tomates cerises en 2 ou laissez-les tel quel si elles sont trop petites.Dans une assiette, déposez au centre les boules de melon tout autour les morceaux d'avocat et terminez par les tomates.",
+		img4Src:"../img/recettes_salades/salade_melon_avocat/assiette.jpg",
+		etape5:"il n'y plus qu'à deguster ...",
+	},
+
+
+
+];
+
+
