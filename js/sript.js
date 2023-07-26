977a3d0378d7ac3b819c787e54d2de448d46041b
herve guerre <herveguerre@hotmail.fr>
2023-07-26 13:51:43 +0200

    m

--- js/sript.js
+++ js/sript.js
@@ -1,11 +1,3 @@
-// Fonction pour récupérer et insérer le contenu du header
-function inclureHeader() {
-	fetch("../../components/header.html")
-	  .then((response) => response.text())
-	  .then((data) => {
-		document.getElementById("header").innerHTML = data;
-	  });
-  }
 // Fonction pour récupérer et insérer le contenu du head
 function inclureHead() {
 	fetch("../../components/head.html")
@@ -14,62 +6,71 @@ function inclureHead() {
 		document.getElementById("head").innerHTML = data;
 	  });
   }
+  
+  // Fonction pour récupérer et insérer le contenu du header
+function inclureHeader() {
+  fetch("../../components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("header").innerHTML = data;
+    });
+}
+
 
 // Fonction pour récupérer et insérer le contenu du liste1
 function inclureList_1() {
-	fetch("../../components/list_1.html")
-	  .then((response) => response.text())
-	  .then((data) => {
-		document.getElementById("list_1").innerHTML = data;
-	  });
-  }
+  fetch("../../components/list_1.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("list_1").innerHTML = data;
+    });
+}
 
-  // Fonction pour récupérer et insérer le contenu du liste2
+// Fonction pour récupérer et insérer le contenu du liste2
 function inclureList_2() {
-	fetch("../../components/list_2.html")
-	  .then((response) => response.text())
-	  .then((data) => {
-		document.getElementById("list_2").innerHTML = data;
-	  });
-  }
+  fetch("../../components/list_2.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("list_2").innerHTML = data;
+    });
+}
 
-    // Fonction pour récupérer et insérer le contenu du liste3
+// Fonction pour récupérer et insérer le contenu du liste3
 function inclureList_3() {
-	fetch("../../components/list_3.html")
-	  .then((response) => response.text())
-	  .then((data) => {
-		document.getElementById("list_3").innerHTML = data;
-	  });
-  }
+  fetch("../../components/list_3.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("list_3").innerHTML = data;
+    });
+}
 
-      // Fonction pour récupérer et insérer le contenu du liste4
+// Fonction pour récupérer et insérer le contenu du liste4
 function inclureList_4() {
-	fetch("../../components/list_4.html")
-	  .then((response) => response.text())
-	  .then((data) => {
-		document.getElementById("list_4").innerHTML = data;
-	  });
-  }
+  fetch("../../components/list_4.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("list_4").innerHTML = data;
+    });
+}
 
-  // Fonction pour récupérer et insérer le contenu du footer
+// Fonction pour récupérer et insérer le contenu du footer
 function inclureFooter() {
-	fetch("../../components/footer.html")
-	  .then((response) => response.text())
-	  .then((data) => {
-		document.getElementById("footer").innerHTML = data;
-	  });
-  }
-  
-    // Fonction pour récupérer et insérer le contenu du bouton
+  fetch("../../components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
+}
+
+// Fonction pour récupérer et insérer le contenu du bouton
 function inclureBouton() {
-	fetch("../../components/bouton.html")
-	  .then((response) => response.text())
-	  .then((data) => {
-		document.getElementById("bouton").innerHTML = data;
-	  });
-  }
-  
-  
+  fetch("../../components/bouton.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("bouton").innerHTML = data;
+    });
+}
+
 // Appel des fonctions pour inclure le header et le footer etc
 inclureHead();
 inclureHeader();
@@ -79,12 +80,8 @@ inclureList_3();
 inclureList_4();
 inclureFooter();
 inclureBouton();
-  
-  // Désactiver l'événement contextmenu (clic droit)
-  //document.addEventListener('contextmenu', function(e) {
-   // e.preventDefault();
-  //});
 
-  
-
-
+// Désactiver l'événement contextmenu (clic droit)
+//document.addEventListener('contextmenu', function(e) {
+// e.preventDefault();
+//});
